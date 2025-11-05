c32fcaf0c34a55f98bc859796af523f8dc2b0f02
tanakanaoki032 <mcd2476032@stu.o-hara.ac.jp>
2025-11-05 10:34:56 +0900

    e

---
+++ src/bean/User.java
@@ -0,0 +1,69 @@
+package model;
+
+public class UserBean {
+    private int userId;
+    private String name;
+    private String email;
+    private String phone;
+    private String password;
+    private String favoriteStore;
+    private int storeId;
+    private boolean notification; // true=ON, false=OFF
+
+    // --- getter/setter ---
+    public int getUserId() {
+        return userId;
+    }
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFavoriteStore() {
+        return favoriteStore;
+    }
+    public void setFavoriteStore(String favoriteStore) {
+        this.favoriteStore = favoriteStore;
+    }
+
+    public int getStoreId() {
+        return storeId;
+    }
+    public void setStoreId(int storeId) {
+        this.storeId = storeId;
+    }
+
+    public boolean isNotification() {
+        return notification;
+    }
+    public void setNotification(boolean notification) {
+        this.notification = notification;
+    }
+}
---
+++ src/dao/UserDAO.java
@@ -0,0 +1,105 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import bean.UserBean;
+
+public class UserDAO {
+
+    private Connection con;
+
+    public UserDAO(Connection con) {
+        this.con = con;
+    }
+
+    // ユーザー登録
+    public void insert(UserBean user) throws SQLException {
+        String sql = "INSERT INTO T004_user (氏名, メールアドレス, 電話番号, パスワード, お気に入り店舗, 店舗ID, 通知ON_OFF) "
+                   + "VALUES (?, ?, ?, ?, ?, ?, ?)";
+        try (PreparedStatement pstmt = con.prepareStatement(sql)) {
+            pstmt.setString(1, user.getName());
+            pstmt.setString(2, user.getEmail());
+            pstmt.setString(3, user.getPhone());
+            pstmt.setString(4, user.getPassword());
+            pstmt.setString(5, user.getFavoriteStore());
+            pstmt.setInt(6, user.getStoreId());
+            pstmt.setBoolean(7, user.isNotification());
+            pstmt.executeUpdate();
+        }
+    }
+
+    // 全件取得
+    public List<UserBean> findAll() throws SQLException {
+        List<UserBean> list = new ArrayList<>();
+        String sql = "SELECT * FROM T004_user ORDER BY ユーザー_ID";
+        try (PreparedStatement pstmt = con.prepareStatement(sql);
+             ResultSet rs = pstmt.executeQuery()) {
+            while (rs.next()) {
+                UserBean user = new UserBean();
+                user.setUserId(rs.getInt("ユーザー_ID"));
+                user.setName(rs.getString("氏名"));
+                user.setEmail(rs.getString("メールアドレス"));
+                user.setPhone(rs.getString("電話番号"));
+                user.setPassword(rs.getString("パスワード"));
+                user.setFavoriteStore(rs.getString("お気に入り店舗"));
+                user.setStoreId(rs.getInt("店舗ID"));
+                user.setNotification(rs.getBoolean("通知ON_OFF"));
+                list.add(user);
+            }
+        }
+        return list;
+    }
+
+    // IDで検索
+    public UserBean findById(int id) throws SQLException {
+        String sql = "SELECT * FROM T004_user WHERE ユーザー_ID = ?";
+        try (PreparedStatement pstmt = con.prepareStatement(sql)) {
+            pstmt.setInt(1, id);
+            ResultSet rs = pstmt.executeQuery();
+            if (rs.next()) {
+                UserBean user = new UserBean();
+                user.setUserId(rs.getInt("ユーザー_ID"));
+                user.setName(rs.getString("氏名"));
+                user.setEmail(rs.getString("メールアドレス"));
+                user.setPhone(rs.getString("電話番号"));
+                user.setPassword(rs.getString("パスワード"));
+                user.setFavoriteStore(rs.getString("お気に入り店舗"));
+                user.setStoreId(rs.getInt("店舗ID"));
+                user.setNotification(rs.getBoolean("通知ON_OFF"));
+                return user;
+            }
+        }
+        return null;
+    }
+
+    // 更新
+    public void update(UserBean user) throws SQLException {
+        String sql = "UPDATE T004_user SET 氏名=?, メールアドレス=?, 電話番号=?, パスワード=?, "
+                   + "お気に入り店舗=?, 店舗ID=?, 通知ON_OFF=? WHERE ユーザー_ID=?";
+        try (PreparedStatement pstmt = con.prepareStatement(sql)) {
+            pstmt.setString(1, user.getName());
+            pstmt.setString(2, user.getEmail());
+            pstmt.setString(3, user.getPhone());
+            pstmt.setString(4, user.getPassword());
+            pstmt.setString(5, user.getFavoriteStore());
+            pstmt.setInt(6, user.getStoreId());
+            pstmt.setBoolean(7, user.isNotification());
+            pstmt.setInt(8, user.getUserId());
+            pstmt.executeUpdate();
+        }
+    }
+
+    // 削除
+    public void delete(int id) throws SQLException {
+        String sql = "DELETE FROM T004_user WHERE ユーザー_ID=?";
+        try (PreparedStatement pstmt = con.prepareStatement(sql)) {
+            pstmt.setInt(1, id);
+            pstmt.executeUpdate();
+        }
+    }
+}
