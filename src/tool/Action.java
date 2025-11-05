d1fae1e2c1fd32498fcad9b712cbbb4d07df99b9
tanakanaoki032 <mcd2476032@stu.o-hara.ac.jp>
2025-11-05 10:37:34 +0900

    a

--- src/tool/Action.java
+++ src/tool/Action.java
@@ -1,5 +1,11 @@
 package tool;
 
-public class Action {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public abstract class Action {
+	public abstract void execute(
+			HttpServletRequest req, HttpServletResponse res
+		) throws Exception;
 
 }
