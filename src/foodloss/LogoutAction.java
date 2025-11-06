377a527d10027f948c2523604b86daf58be1b36a
mkblue82 <mmr2482lk@gmail.com>
2025-11-06 10:15:40 +0900

    ログアウトアクション

--- src/foodloss/LogoutAction.java
+++ src/foodloss/LogoutAction.java
@@ -1,8 +1,20 @@
 package foodloss;
 
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import tool.Action;
 
 @WebServlet("/logout")
-public class LogoutAction {
-	
+public class LogoutAction extends Action{
+	public void execute(HttpServletRequest req, HttpServletResponse res) throws Exception{
+		HttpSession session = req.getSession(false);
+		if (session != null){
+			session.invalidate();
+		}
+
+		req.getRequestDispatcher("jsp/logout_done.jsp").forward(req, res);
+	}
 }
