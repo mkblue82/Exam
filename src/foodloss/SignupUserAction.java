a5f1ea23d1a5c9820e8d34bf54e25450d7f7a0fe
tamon7 <mcd2476082@stu.o-hara.ac.jp>
2025-11-10 14:37:07 +0900

    u

--- src/foodloss/SignupUserAction.java
+++ src/foodloss/SignupUserAction.java
@@ -17,7 +17,7 @@ import bean.User;
 import dao.UserDAO;
 import tool.DBManager;
 
-@WebServlet("/ignupUser.action")
+@WebServlet("/signupUser")
 public class SignupUserAction extends HttpServlet {
 
     @Override
