a8a7872ba9a441077951e30b8c5d7b10271820b1
mkblue82 <mmr2482lk@gmail.com>
2025-11-12 10:47:47 +0900

    syuusei

--- src/foodloss/LoginExecuteAction.java
+++ src/foodloss/LoginExecuteAction.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import bean.User;
 import dao.UserDAO;
 import tool.Action;
 import tool.DBManager;
