7c30c0bd937bf94149712ecd5fc3b82963d0a3c0
mkblue82 <mmr2482lk@gmail.com>
2025-11-14 11:38:49 +0900

    menu.action

--- src/foodloss/MenuAction.java
+++ src/foodloss/MenuAction.java
@@ -1,17 +1,30 @@
 package foodloss;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import tool.Action;
 
 public class MenuAction extends Action {
-
 	@Override
 	public void execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
+		HttpSession session = req.getSession();
 
-		// 取りあえずはmain_user.jspにフォワード先
-		// ログイン出来るようになったら、ログインユーザーの属性に合わせてフォワード先変わるようにする
-		req.getRequestDispatcher("/jsp/main_user.jsp").forward(req, res);
+		// セッションから店舗情報とユーザー情報を取得
+		Object store = session.getAttribute("store");
+		Object user = session.getAttribute("user");
+
+		// 店舗としてログインしている場合
+		if (store != null) {
+			req.getRequestDispatcher("/jsp/main_store.jsp").forward(req, res);
+		}
+		// 一般ユーザーとしてログインしている場合
+		else if (user != null) {
+			req.getRequestDispatcher("/jsp/main_user.jsp").forward(req, res);
+		}
+		// ログインしていない場合（念のため）
+		else {
+			res.sendRedirect(req.getContextPath() + "/jsp/index.jsp");
+		}
 	}
 }
